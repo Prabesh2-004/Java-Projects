81220e79a4f8a75a0bbbc81b0fe4cd17f35d9a76
Prabesh2-004 <coder.bro2004@gmail.com>
2026-08-19 22:15:32 +0545

    polished Number Guessing Game

--- src/NumberGuessingGame/Main.java
+++ src/NumberGuessingGame/Main.java
@@ -4,107 +4,9 @@ import java.util.Scanner;
 
 public class Main {
 
-    public void Easy() {
-        int randomNumber = (int) (Math.random() * 10) + 1;
-        int numberOfGuess = 0;
-
-        Scanner scanner = new Scanner(System.in);
-
-        int guesses;
-        try {
-            do {
-                System.out.print("Enter your guessed number 1 to 10: ");
-                guesses = scanner.nextInt();
-                if (guesses > 10 | guesses < 1) {
-                    System.out.println("Please enter a number 1 to 10");
-                } else {
-                    numberOfGuess++;
-                    if (randomNumber > guesses) {
-                        System.out.println("Low");
-                    } else if (randomNumber < guesses) {
-                        System.out.println("High");
-                    } else {
-                        System.out.println("Correct Guess! It was " + randomNumber + " and you took " + numberOfGuess + " attempts");
-                    }
-                }
-            } while (guesses != randomNumber);
-        } catch (Exception e) {
-            System.out.println("Exception " + e);
-        }
-
-
-    }
-
-    public void Medium() {
-        int randomNumber = (int) (Math.random() * 50) + 1;
-        int numberOfGuess = 0;
-
-        Scanner scanner = new Scanner(System.in);
-
-        int guesses;
-        try {
-            do {
-                System.out.print("Enter your guessed number 1 to 50: ");
-                guesses = scanner.nextInt();
-                if (guesses > 50 | guesses < 1) {
-                    System.out.println("Please enter a number 1 to 50");
-                } else {
-                    numberOfGuess++;
-                    if (randomNumber > guesses) {
-                        System.out.println("Low");
-                    } else if (randomNumber < guesses) {
-                        System.out.println("High");
-                    } else {
-                        System.out.println("Correct Guess! It was " + randomNumber + " and you took " + numberOfGuess + " attempts");
-                    }
-                }
-            } while (guesses != randomNumber);
-        } catch (Exception e) {
-            System.out.println("Exception " + e);
-        }
-
-
-    }
-
-    public void High() {
-        int randomNumber = (int) (Math.random() * 100) + 1;
-        int numberOfGuess = 0;
-        Scanner scanner = new Scanner(System.in);
-
-        int guesses;
-        try {
-            do {
-                System.out.print("Enter your guessed number 1 to 100: ");
-                guesses = scanner.nextInt();
-                if (guesses > 100 | guesses < 1) {
-                    System.out.println("Please enter a number 1 to 100");
-                } else {
-                    numberOfGuess++;
-                    if (randomNumber > guesses) {
-                        System.out.println("Low");
-                    } else if (randomNumber < guesses) {
-                        System.out.println("High");
-                    } else {
-                        System.out.println("Correct Guess! It was " + randomNumber + " and you took " + numberOfGuess + " attempts");
-                    }
-                }
-            } while (guesses != randomNumber);
-        } catch (Exception e) {
-            System.out.println("Exception " + e);
-        }
-
-    }
-
-    public static void main(String[] args) {
+    public void startGame(Scanner scanner) {
         boolean isPlaying = true;
 
-        Main main = new Main();
-
-        System.out.println("---------------------------------");
-        System.out.println(" Welcome to number guessing Game ");
-        System.out.println("---------------------------------");
-
-        Scanner scanner = new Scanner(System.in);
 
         while (isPlaying) {
             System.out.println();
@@ -113,21 +15,81 @@ public class Main {
             System.out.println("3. Hard");
             System.out.println("4. Exit");
             System.out.print("choose a Level: ");
-            int userChoice = scanner.nextInt();
-            System.out.println();
             try {
-                switch (userChoice) {
-                    case 1 -> main.Easy();
-                    case 2 -> main.Medium();
-                    case 3 -> main.High();
-                    case 4 -> isPlaying = false;
-                    default -> System.out.println("Please enter a number that are specified above. ");
+                if (scanner.hasNextInt()) {
+                    int userChoice = scanner.nextInt();
+                    switch (userChoice) {
+                        case 1, 2, 3 -> gameLevel(userChoice, scanner);
+                        case 4 -> isPlaying = false;
+                        default -> System.out.println("Please enter a number that are specified above. ");
+                    }
+                } else {
+                    String invalidInput = scanner.next();
+                    System.out.println("Invalid input: " + invalidInput + " . Please enter a valid number (1-4).");
+                    throw new IllegalArgumentException("Invalid input provided: " + invalidInput);
                 }
             } catch (Exception e) {
-                System.out.println("Exception " + e);
+                System.out.println("Exception " + e.getMessage());
             }
 
         }
+    }
+
+    public void gameLevel(int difficulty, Scanner scanner) {
+        int easyMaxAttempt = 15, mediumMaxAttempt = 10, hardMaxAttempt = 5;
+
+        switch (difficulty) {
+            case 1 -> playGame(scanner, easyMaxAttempt);
+            case 2 -> playGame(scanner, mediumMaxAttempt);
+            case 3 -> playGame(scanner, hardMaxAttempt);
+            default -> System.out.println("Invalid Input");
+        }
+    }
+
+    public void playGame(Scanner scanner, int maxAttempts) {
+        int randomNumber = (int) (Math.random() * 100) + 1;
+        try {
+            int attemptCount = 0;
+            int guesses;
+            do {
+                System.out.print("Enter your guessed number 1 to 100: ");
+                if (scanner.hasNextInt()) {
+                    guesses = scanner.nextInt();
+                    if (guesses > 100 || guesses < 1) {
+                        System.out.println("Please enter a number 1 to 100");
+                    } else {
+                        attemptCount++;
+                        if (randomNumber > guesses) {
+                            System.out.println("Low");
+                        } else if (randomNumber < guesses) {
+                            System.out.println("High");
+                        }
+                        if (guesses == randomNumber) {
+                            System.out.println("Correct Guess! It was " + randomNumber + " and you took " + attemptCount + " attempts"
+                            );
+                        } else if (maxAttempts == attemptCount){
+                            System.out.println("Out of Attempts. " + randomNumber + " was actual answer.");
+                        }
+                    }
+                } else {
+                    String invalidInput = scanner.next();
+                    System.out.println("Invalid input: " + invalidInput + " . Please enter a valid number (1-100).");
+                    throw new IllegalArgumentException("Invalid input provided: " + invalidInput);
+                }
+            } while (guesses != randomNumber && maxAttempts != attemptCount);
+        } catch (Exception e) {
+            System.out.println("Exception " + e);
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("---------------------------------");
+        System.out.println(" Welcome to number guessing Game ");
+        System.out.println("---------------------------------");
+
+        Scanner scanner = new Scanner(System.in);
+        Main main = new Main();
+        main.startGame(scanner);
         scanner.close();
     }
 }
