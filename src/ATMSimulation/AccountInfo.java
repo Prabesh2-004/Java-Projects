8514ed8031371f34212edcc803ac86b2c6d766fc
Prabesh2-004 <coder.bro2004@gmail.com>
2026-08-20 17:47:50 +0545

    DRY improvement and code Polished

--- src/ATMSimulation/AccountInfo.java
+++ src/ATMSimulation/AccountInfo.java
@@ -4,19 +4,29 @@ public class AccountInfo {
     private double accountBalance = 0;
     private final int accountPin = 1234;
 
-    public double balance() {
+    public double getBalance() {
         return accountBalance;
     }
 
-    public int pin() {
+    public int getPin() {
         return accountPin;
     }
 
     public void deposit(double amount) {
-        accountBalance += amount;
+        if (amount > 0) {
+            accountBalance += amount;
+        } else {
+            System.out.println("Invalid Amount");
+        }
     }
 
     public void withdraw(double amount) {
-        accountBalance -= amount;
+        if (amount > accountBalance) {
+            System.out.println("Insufficient Balance");
+        } else if (amount < 1) {
+            System.out.println("Invalid balance");
+        } else {
+            accountBalance -= amount;
+        }
     }
 }
--- src/ATMSimulation/PerformAction.java
+++ src/ATMSimulation/PerformAction.java
@@ -4,37 +4,29 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class PerformAction {
-    private void balanceDeposit(AccountInfo accountInfo, Scanner scanner) throws InputMismatchException {
-        System.out.print("Please enter how much you want to deposit: ");
-        double amount = scanner.nextDouble();
-        if (amount < 1) {
-            System.out.println("Cannot deposit this much amount");
-        } else {
-            accountInfo.deposit(amount);
+    private void balanceModify(AccountInfo accountInfo, Scanner scanner, int userChoice) {
+        System.out.print("Please enter how much you want to " + (userChoice == 2 ? "deposit: " : userChoice == 3 ? "withdraw: " : "Invalid Input"));
+        try {
+            if (scanner.hasNextDouble()) {
+                double amount = scanner.nextDouble();
+                if (userChoice == 2) {
+                    accountInfo.deposit(amount);
+                } else if (userChoice == 3){
+                    accountInfo.withdraw(amount);
+                } else {
+                    System.out.println("Invalid Input");
+                }
+            } else {
+                String invalidInput = scanner.next();
+                System.out.println("Invalid Input \"" + invalidInput + "\"");
+            }
+        } catch (InputMismatchException e) {
+            System.out.println(e.getMessage());
         }
     }
 
-    private void balanceWithdraw(AccountInfo accountInfo, Scanner scanner) throws InputMismatchException {
-        System.out.print("Please enter how much you want to withdraw: ");
-        double amount = scanner.nextDouble();
-        if (amount > accountInfo.balance()) {
-            System.out.println("Insufficient Balance");
-        } else if (amount < 1) {
-            System.out.println("This amount cannot be withdraw");
-        } else {
-            accountInfo.withdraw(amount);
-        }
-    }
 
-    private void checkPIN(int userPIN, AccountInfo accountInfo, Scanner scanner) throws InputMismatchException {
-        if (userPIN == accountInfo.pin()) {
-            showOption(accountInfo, scanner);
-        } else {
-            System.out.println("Invalid PIN! Please try again");
-        }
-    }
-
-    private void showOption(AccountInfo accountInfo, Scanner scanner) throws InputMismatchException {
+    private void showOption(AccountInfo accountInfo, Scanner scanner) {
         boolean isATMStarted = true;
         do {
             System.out.println("1. Check Balance");
@@ -42,32 +34,63 @@ public class PerformAction {
             System.out.println("3. Withdraw Balance");
             System.out.println("4. Exit");
             System.out.print("Please Enter what you want to perform: ");
-            int userChoice = scanner.nextInt();
-            System.out.println();
-            switch (userChoice) {
-                case 1 -> System.out.println(accountInfo.balance());
-                case 2 -> balanceDeposit(accountInfo, scanner);
-                case 3 -> balanceWithdraw(accountInfo, scanner);
-                case 4 -> isATMStarted = false;
-                default -> System.out.println("Please enter specified number only");
+            try {
+                if (scanner.hasNextInt()) {
+                    int userChoice = scanner.nextInt();
+                    System.out.println();
+                    switch (userChoice) {
+                        case 1 -> System.out.println(accountInfo.getBalance());
+                        case 2, 3 -> balanceModify(accountInfo, scanner, userChoice);
+                        case 4 -> isATMStarted = false;
+                        default -> System.out.println("Please enter specified number only");
+                    }
+                } else {
+                    String invalidInput = scanner.next();
+                    System.out.println("Invalid Input \"" + invalidInput + "\"");
+                }
+            } catch (InputMismatchException e) {
+                System.out.println(e.getMessage());
             }
 
             System.out.println();
         } while (isATMStarted);
     }
 
-    public void startATM(AccountInfo accountInfo) throws InputMismatchException {
+    public void startATM(AccountInfo accountInfo) {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("-------------------------");
         System.out.println(" Welcome to SanuBank ATM ");
         System.out.println("-------------------------");
         System.out.println();
-        System.out.print("Please enter you PIN: ");
-        int userEnteredPin = scanner.nextInt();
 
-        checkPIN(userEnteredPin, accountInfo, scanner);
+        int pinEnter = 0;
+        boolean isPinIncorrect = true;
+        do {
+            System.out.print("Please enter you PIN: ");
+            try {
+                if (scanner.hasNextInt()) {
+                    pinEnter++;
+                    int userEnteredPin = scanner.nextInt();
+                    if (userEnteredPin == accountInfo.getPin()) {
+                        isPinIncorrect = false;
+                        showOption(accountInfo, scanner);
+                    } else {
+                        if (pinEnter == 3) {
+                            System.out.println("Maximum attempt used please try again later. ");
+                        } else {
+                            System.out.println("Invalid PIN! Please try again");
+                        }
+                    }
+                } else {
+                    String invalidInput = scanner.next();
+                    System.out.println("Invalid Input \"" + invalidInput + "\" value must be in Integer");
+                }
+            } catch (InputMismatchException e) {
+                System.out.println(e.getMessage());
+            }
+        } while (pinEnter != 3 && isPinIncorrect);
 
-
+        scanner.close();
     }
 }
