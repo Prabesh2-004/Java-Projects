bd22d9f4d0ed15f893819c39e5daaba9b1aed277
Prabesh2-004 <coder.bro2004@gmail.com>
2026-09-02 20:45:08 +0545

    add and get all product method added

--- src/InventoryManagementSystem/AssetsManagement.java
+++ src/InventoryManagementSystem/AssetsManagement.java
@@ -1,4 +1,98 @@
 package InventoryManagementSystem;
 
+import java.util.Scanner;
+
 public class AssetsManagement {
+    int currentIndexPosition = 0;
+    private int productId = 0;
+
+    public void addProduct(AssetsStorage[] assetsStorages, Scanner scanner) {
+        boolean isValid = true;
+        String name = "";
+        double price = 0;
+        int quantity = 0;
+
+        System.out.print("Set Name for a product: ");
+        name = scanner.nextLine();
+        while (isValid) {
+            System.out.print("Set a price for the product: ");
+            if (scanner.hasNextDouble()) {
+                price = scanner.nextDouble();
+                isValid = false;
+            } else {
+                String invalidInput = scanner.next();
+                System.out.println(invalidInput + ": is invalid price type");
+            }
+
+        }
+
+        isValid = true;
+        while (isValid) {
+            System.out.print("Set a quantity of product: ");
+            if (scanner.hasNextInt()) {
+                quantity = scanner.nextInt();
+                isValid = false;
+            } else {
+                String invalidInput = scanner.next();
+                System.out.println(invalidInput + ": is a invalid quantity type");
+            }
+        }
+
+        if (assetsStorages.length == currentIndexPosition) {
+            System.out.println("Out of memory");
+        } else {
+            productId++;
+            assetsStorages[currentIndexPosition] = new AssetsStorage(productId, name, price, quantity);
+            currentIndexPosition++;
+        }
+    }
+
+    public void getAllProduct(AssetsStorage[] assetsStorages) {
+        for (int i = 0; i < currentIndexPosition; i++) {
+            System.out.println(assetsStorages[i].getId() + " " + assetsStorages[i].getName() + " " + assetsStorages[i].getQuantity() + " " + assetsStorages[i].getPrice());
+        }
+    }
+
+    public void launcher() {
+        Scanner scanner = new Scanner(System.in);
+        boolean isRunning = true;
+
+        AssetsStorage[] assetsStorages = new AssetsStorage[10];
+
+        while (isRunning) {
+            System.out.println();
+            System.out.println("1. Add Product");
+            System.out.println("2. Get All Products");
+            System.out.println("3. Search Product");
+            System.out.println("4. Update Product");
+            System.out.println("5. Add Stock");
+            System.out.println("6. Sell Product");
+            System.out.println("7. Delete Product");
+            System.out.println("8. Get Low Stock Products");
+            System.out.println("9. Exit");
+
+            System.out.print("Please choose an operation: ");
+            if (scanner.hasNextInt()) {
+                int userChoice = scanner.nextInt();
+                scanner.nextLine();
+
+                switch (userChoice) {
+                    case 1 -> addProduct(assetsStorages, scanner);
+                    case 2 -> getAllProduct(assetsStorages);
+                    case 3 -> System.out.println("Choice Three");
+                    case 4 -> System.out.println("Choice Four");
+                    case 5 -> System.out.println("Choice Five");
+                    case 6 -> System.out.println("Choice Six");
+                    case 7 -> System.out.println("Choice Seven");
+                    case 8 -> System.out.println("Choice Eight");
+                    case 9 -> isRunning = false;
+                    default -> System.out.println("Invalid choice please choose listed number only");
+                }
+            } else {
+                String invalidInput = scanner.next();
+                System.out.println("Invalid input choose one the option");
+            }
+        }
+
+    }
 }
--- src/InventoryManagementSystem/Main.java
+++ src/InventoryManagementSystem/Main.java
@@ -2,6 +2,7 @@ package InventoryManagementSystem;
 
 public class Main {
     public  static void main(String[] args) {
-
+        AssetsManagement assetsManagement = new AssetsManagement();
+        assetsManagement.launcher();
     }
 }
